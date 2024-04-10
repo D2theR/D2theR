365130afc89c541a9f5f97a09d0ef22153a23830
Dan Rerko <D2theR@users.noreply.github.com>
2024-04-10 22:12:29 +0000

    Updating Careers module to test deploy/rebuild.

--- components/Careers.vue
+++ components/Careers.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="p-6 rounded-lg shadow bg-white dark:bg-gray-800">
-    <h2 class="text-xl font-bold text-gray-800 dark:text-white mb-4">Career Path</h2>
+    <h2 class="text-xl font-bold text-gray-800 dark:text-white mb-4">Skills</h2>
     <section class="mb-4">
-      <h3 class="font-semibold text-gray-700 dark:text-gray-200">Polychem Dispersions Inc. </h3>
+      <h3 class="font-semibold text-gray-700 dark:text-gray-200">|<>|</h3>
       </section>
   </div>
 </template>
