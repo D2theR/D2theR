21b669f120f11bd2b31e693e3aed0a55cf20c707
Dan Rerko <D2theR@users.noreply.github.com>
2024-04-10 21:35:25 +0000

    lets try messing with baseURL again...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    baseURL: '/D2theR/'
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui','@pinia/nuxt', '@nuxtjs/color-mode'],
   css: ['~/assets/css/main.css',],
