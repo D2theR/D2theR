19b2d559756078ab15d1f6c22131815832811945
Dan Rerko <D2theR@users.noreply.github.com>
2024-04-10 19:12:23 +0000

    change buildAssetsDir to fix gh-page

--- d2ther/nuxt.config.ts
+++ d2ther/nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
+    app:{
+      buildAssetsDir:'.'
+    },
     public: {
-      baseURL: '/D2theR/' 
+      baseURL: process.env.NUXT_PUBLIC_BASE_URL || '/',
     }
   },
   devtools: { enabled: true },
